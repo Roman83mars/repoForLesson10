89c82062d9a864798de4ee3fdd22400d701b87c2
Roman Amb <roman83mars@gmail.com>
2024-03-10 13:22:16 +0300

    задание десятого урока

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
 const books = document.querySelectorAll('.book')
 const body = document.querySelector('body')
-const title3 = document.querySelectorAll('.book h2')[4]
+const title3 = document.querySelectorAll('.book h2 a')[4]
+const advertisement = document.querySelector('.adv')
+const chapters = document.querySelectorAll('.book li')
+const newChapters = document.createElement('li')
 
 books[2].before(books[5])
 books[5].before(books[3])
@@ -12,4 +15,18 @@ books[0].before(books[1])
 
 body.style.setProperty('background-image', 'url(./image/you-dont-know-js.jpg)')
 
-console.dir(title3);
+title3.textContent = 'Книга 3. this и Прототипы Объектов'
+
+advertisement.remove()
+
+chapters[2].before(chapters[3])
+chapters[3].after(chapters[6])
+chapters[6].after(chapters[8])
+chapters[10].before(chapters[2])
+chapters[49].before(chapters[55])
+chapters[50].after(chapters[48])
+chapters[54].before(chapters[51])
+
+newChapters.textContent = 'Глава 8: За пределами ES6'
+chapters[26].insertAdjacentElement('beforebegin', newChapters)
+
